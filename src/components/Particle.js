df9a87cf994d7b8fcb12e86ea4465b190b678443
ManishKumarGeek <manish.gamedev1@gmail.com>
2024-06-07 00:35:47 +0530

    initial commit

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,55 +1,68 @@
-import React from "react";
-import Particles from "react-tsparticles";
+import React from 'react';
+import Particles from 'react-tsparticles';
 
-function Particle() {
-  return (
-    <Particles
-      id="tsparticles"
-      params={{
-        particles: {
-          number: {
-            value: 160,
-            density: {
-              enable: true,
-              value_area: 1750,
-            },
-          },
-          line_linked: {
-            enable: false,
-            opacity: 0.03,
-          },
-          move: {
-            direction: "right",
-            speed: 0.05,
-          },
-          size: {
-            value: 1,
-          },
-          opacity: {
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0.05,
-            },
-          },
+const FireParticles = () => {
+  const particlesConfig = {
+    fullScreen: {
+      enable: true,
+      zIndex: -1 // Position particles behind content
+    },
+    fpsLimit: 60,
+    particles: {
+      number: {
+        value: 50
+      },
+      color: {
+        value: ["#fdcf58", "#757676", "#f27d0c", "#800909", "#f07f13"]
+      },
+      opacity: {
+        value: 0.5,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 1,
+          opacity_min: 0.25,
+          sync: false
+        }
+      },
+      size: {
+        value: 4,
+        random: true
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "top",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        attract: {
+          enable: false
+        }
+      },
+      shape: {
+        type: "circle"
+      },
+      line_linked: {
+        enable: false
+      }
+    },
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onHover: {
+          enable: false
         },
-        interactivity: {
-          events: {
-            onclick: {
-              enable: true,
-              mode: "push",
-            },
-          },
-          modes: {
-            push: {
-              particles_nb: 1,
-            },
-          },
+        onClick: {
+          enable: false
         },
-        retina_detect: true,
-      }}
-    />
-  );
-}
+        resize: true
+      }
+    },
+    retina_detect: true
+  };
 
-export default Particle;
+  return <Particles id="tsparticles" options={particlesConfig} />;
+};
+
+export default FireParticles;
